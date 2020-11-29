b34267de73ca4a5099f719f7af05086d6b1f0b0a
Araisite <66276245+Araisite@users.noreply.github.com>
2020-11-28 20:41:54 -0500

    tout dans un package

--- test/src/test/test/Principal.java
+++ test/src/test/test/Principal.java
@@ -1,9 +1,10 @@
+package test;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class principal {
+public class Principal {
 
 	/*public static void main(String[] args) {
 		
