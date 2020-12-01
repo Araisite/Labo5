c68fb6c9f27598c0e5d443cbb7670ae3bb2980b8
QcMacpoulet <75143004+QcMacpoulet@users.noreply.github.com>
2020-12-01 16:15:42 -0500

    new lexinode

--- test/src/test/LexiNode.java
+++ test/src/test/LexiNode.java
@@ -1,80 +1,80 @@
-package test;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class LexiNode {
 	
-	private char noeud;
-	private char nextNoeud;
+	private char lettreAcutelle;
 	private String definition;
-	private LinkedList <LexiNode> enfants = new LinkedList <LexiNode> ();
-	private Dictionnaire dictionnaire;
+	private String motCourant;
+	private ArrayList<LexiNode> enfants;
 
 	
 	
-	public LexiNode (char noeud, Dictionnaire dictionnaire, int index) {
-		
-		System.out.println(noeud);
-		this.noeud = noeud;
-		this.dictionnaire = dictionnaire;
-	
-		
-		//Dictionnaire avec seulement les mots utiles
-		dictionnaire.updateDictionnaire(noeud, index);
-		
-		index=index+1;
-		
-		//Parcours la liste de mots restants
-		for (int i=0; i<dictionnaire.getNbrMots(); i++) {
-			
-			//Si mot complet, ajoute la definition
-			if (index == dictionnaire.getMot(i).length()) {
-				
-				definition = dictionnaire.getDefinition(i);
-			}
-			
-			//Si la lettre suivante pas deja un enfant, l'ajouter	
-			if (enfants.contains(dictionnaire.getMot(i).charAt(index)) == false) {
-				
-				LexiNode enfantTemp = new LexiNode (dictionnaire.getMot(i).charAt(index), dictionnaire, index);
-				enfants.add(enfantTemp);
-				
-			}			
-		}		
-	}
+	public LexiNode (char lettreAcutelle, String motCourant, String definition) {
 
-
-	public char getNoeud() {
-		return noeud;
-	}
-
-
-	public void setNoeud(char noeud) {
-		this.noeud = noeud;
-	}
-
-
-	public String getDefinition() {
-		return definition;
-	}
-
-
-	public void setDefinition(String definition) {
+		this.lettreAcutelle = lettreAcutelle;
+		this.motCourant = motCourant;
 		this.definition = definition;
-	}
-
-
-	public LinkedList<LexiNode> getEnfants() {
-		return enfants;
-	}
-
-
-	public void setEnfants(LinkedList<LexiNode> enfants) {
-		this.enfants = enfants;
+		
+		
 	}
 	
-	public Dictionnaire getDictionnaire() {
-		return dictionnaire;
+	public void ajouterMot (String mot, String definition) {
+		
+		
+		char lettreCourante = mot.charAt(0);
+		String defTemp;
+		
+		for (int i=0; i<enfants.size(); i++) {
+			
+			if(enfants.get(i).lettreAcutelle == lettreCourante) {
+				
+				enfants.get(i).ajouterMot(mot.substring(1), definition);
+				return;
+			}
+		}
+			
+		if (mot.substring(1)!="") {
+			
+			defTemp = "";
+		}
+		else {
+			
+			defTemp = definition;
+		}
+		
+		LexiNode noeud = new LexiNode(lettreCourante, motCourant+lettreCourante, defTemp);
+		enfants.add(noeud);
+				
+		if (mot.substring(1) !="") {
+			
+			noeud.ajouterMot(mot.substring(1), definition);
+		}	
 	}
 	
+	public void modifierMot (String mot) {
+		
+		
+	}
+	
+	public void printChoix(String mot) {
+		
+		char lettreCourante = mot.charAt(0);
+		
+		for (int i=0; i<enfants.size(); i++) {
+			
+			if(enfants.get(i).lettreAcutelle == lettreCourante) {
+				
+				System.out.print(lettreCourante);
+				enfants.get(i).printChoix(mot.substring(1));
+				
+				return;
+			}
+		}
+		
+		
+	}
+
+		 
+	}
 }
