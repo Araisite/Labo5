5c9a0f81dbc273f1f1d38bf5119e870f42eb18d5
Araisite <66276245+Araisite@users.noreply.github.com>
2020-12-06 23:41:17 -0500

    si j'aurai su la merde que c'est ce labo, j'aurai pas fait ca

--- test/src/module-info.java
+++ test/src/module-info.java
@@ -1,4 +1,5 @@
 module test {
 	requires java.desktop;
     requires java.logging;
+    requires org.junit.jupiter.api;
 }
---
+++ test/src/test/test/mainLab/testLexiNode.java
@@ -0,0 +1,18 @@
+package mainLab;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+
+ @author Philippe Meuser, Corentin Seguin
+ @version 1.0
+ @since 2020/12/06
+ */
+public class testLexiNode {
+
+    @Test
+    void testAjouterMot(){
+        Assertions.assertEquals(1, 1);
+    }
+}
